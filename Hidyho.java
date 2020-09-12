1abb89b18bd1add2a1f5d4586d5f0bca414da3eb
ray_f <ray_fitzpatrick@hotmail.com>
2020-09-12 02:42:16 +0100

    Testing git commit

--- Hidyho.java
+++ Hidyho.java
@@ -9,13 +9,13 @@ public class Hidyho {
 	
 		
 		Scanner in = new Scanner (System.in);
-		System.out.print("Please enter a number between 1000 & 999,999: ");
+		System.out.print("Please enter a number between 1000 & 999,998: ");
 		String myNumber = in.next();
 		String front = myNumber.substring(0,myNumber.length()-3);
 		String end = myNumber.substring(myNumber.length()-3);
 		System.out.println(front+end);
 		
-		
+		// testing for commit
 		
 	}
 
